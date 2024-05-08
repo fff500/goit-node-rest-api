5092da02ac87f4f06ac65095f79f6479f4b48947
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-05-08 13:10:07 +0300

    getAllContacts controller was added

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,13 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsServices from '../services/contactsServices.js';
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const result = await contactsServices.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getOneContact = (req, res) => {};
 
